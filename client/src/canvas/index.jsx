b24f8671d317c71e4e269b5d94ab958ffe0d8184
lepic974 <johan.lepinay@gmail.com>
2023-04-01 11:44:51 +0400

    first commit

---
+++ client/src/canvas/index.jsx
@@ -0,0 +1,13 @@
+import { Canvas } from 'react-three-fiber';
+import CameraRig from './CameraRig';
+import Backdrop from './Backdrop';
+import Shirt from './Shirt';
+import { Environment } from '@react-three/drei';
+
+
+const CanvasModel = () => {
+  return <div>index</div>
+
+};
+
+export default index;
